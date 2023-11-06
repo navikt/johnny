57de3988eb7660bc6ee96835855ac6739bf73379
Johnny Horvi <johnny@horvi.no>
2023-11-06 17:29:00 +0100

    test

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 // Run http server that prints authorization header
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header.Get("Authorization"))
 		fmt.Fprintf(w, "Authorization: %s", r.Header.Get("Authorization"))
 	})
 
