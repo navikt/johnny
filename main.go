b237f9d216bdba61c479623f8aa699e9c1158c40
Johnny Horvi <johnny@horvi.no>
2023-11-06 18:08:19 +0100

    test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -9,6 +10,14 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Header.Get("Authorization"))
+		// print payload to log
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Payload:")
+		fmt.Println(string(b))
+
 		fmt.Fprintf(w, "Authorization: %s", r.Header.Get("Authorization"))
 	})
 
